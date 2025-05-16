337a1527cd204223f56deeba00e31c8e3ea63e9b
malekamk <kganyamilton@gmail.com>
2025-05-16 09:46:13 +0200

    feat: add getAllusers- returns a list of available users

--- src/main/java/com/example/demo/repo/FakeRepoInterface.java
+++ src/main/java/com/example/demo/repo/FakeRepoInterface.java
@@ -2,8 +2,11 @@ package com.example.demo.repo;
 
 import com.example.demo.model.User;
 
+import java.util.List;
+
 public interface FakeRepoInterface {
     void insertUser(User user);
     User findUserById(int id);
     void deleteUser(int id);
+    List<User> getAllUsers();
 }
